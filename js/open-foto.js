1c0967477dd8650b639bd7e8e6978e48b8ffc5c3
Utah <nikitenkogv.main@gmail.com>
2017-06-12 23:22:25 +0300

    Загрузка билда на тестовый продакшн

--- js/open-foto.js
+++ js/open-foto.js
@@ -7,8 +7,13 @@ elements = [].slice.apply(elements); // toArray
 elements.forEach(function (element, i) {
 
     element.addEventListener('click', function () {
-      
-      element.classList.toggle("main-photo__img--click");
+      /*element.classList.toggle("main-photo__img--click");*/
+        if (element.classList.contains('main-photo__img-vertical')) {
+            element.classList.toggle("main-photo__img-vertical--click");
+        }
+        else {
+            element.classList.toggle("main-photo__img--click");
+        }
 
     })
 
